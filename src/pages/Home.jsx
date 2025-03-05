3f7752d791c73f2903ee85649b75834668e1b183
Ivan <ivan.kels@gmail.com>
2025-03-05 13:59:09 +0100

    added card and main section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "../components/Hero";
 import Subscription from "../components/Subscription";
+import MainSection from "../components/MainSection";
 
 const Home = () => {
   return (
     <div>
       <Hero />
+      <MainSection />
       <section>
         <Subscription />
       </section>
