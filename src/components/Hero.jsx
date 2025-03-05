20c299f4256bdc9999af8b57d6934aee1d3cfb68
Johanna <johanna.k.1996@googlemail.com>
2025-03-05 15:29:30 +0100

    design upgrade homepage

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,19 +2,15 @@ import { Link } from "react-router";
 
 const Hero = () => {
   return (
-    <div className="flex flex-col gap-8 min-h-3/4 bg-[#FEFDF8] pt-12 justify-center items-center">
+    <div className="flex flex-col gap-8 min-h-screen bg-[linear-gradient(to_top,rgb(30,35,42),transparent),url('https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-no-repeat bg-top pt-12 justify-start items-center">
       <h1 className="font-Snippet text-5xl text-lightGreen uppercase tracking-wider text-center px-10">
         Explore the lands beyond
       </h1>
       <Link to={"/posts/create"}>
-        <button className="btn bg-darkGreen hover:bg-orange border-none text-lg font-Paytone text-lightBeige font-extralight lowercase tracking-widest">
+        <button className="px-4 py-2 rounded-2xl border border-darkGreen hover:border-orange hover:text-orange text-lg  text-darkGreen font-extralight uppercase tracking-widest">
           create post
         </button>
       </Link>
-      <img
-        src="https://images.unsplash.com/photo-1609342122563-a43ac8917a3a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-      />
     </div>
   );
 };
